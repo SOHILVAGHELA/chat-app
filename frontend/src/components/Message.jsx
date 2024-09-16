91120ad4b0341a9e5066670e1d38d634d666ebd9
SOHILVAGHELA <sohilvaghela7863@gmail.com>
2024-09-16 09:48:42 +0530

    change default scoll bahavior and add some validation on maggagecontainer

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { useSelector } from "react-redux";
 
 function Message({ message }) {
+  const scroll = useRef();
+  useEffect(() => {
+    scroll.current?.scrollIntoView({ behavior: "smooth" });
+  }, [message]);
+  const { authUser, selectedUser } = useSelector((store) => store.user);
   return (
-    <div className="chat chat-end">
+    <div
+      ref={scroll}
+      className={`chat ${
+        authUser?._id === message?.senderId ? "chat-end" : "chat-start"
+      }`}
+    >
       <div className="chat-image avatar">
         <div className="w-10 rounded-full">
           <img
-            alt="Tailwind CSS chat bubble component"
-            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+            alt="user-profile"
+            src={
+              authUser?._id === message?.senderId
+                ? authUser?.profilePhoto
+                : selectedUser?.profilePhoto
+            }
           />
         </div>
       </div>
